b7be0b90e4701d67788e0fd0ec552e151fcb0ede
Pink727 <doc72789@gmail.com>
2025-01-21 14:58:05 -0800

    add support for dynamic path resolution in server file and update route handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
@@ -17,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 // All other routes should serve the index.html file
-app.get('*', (_, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
